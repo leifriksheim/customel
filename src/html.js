f09822709141639a136b794c58d00225268b36a1
leifriksheim <riksheim.leif@gmail.com>
2019-06-04 22:23:17 -0500

    if null is an arg, don't render it

--- src/html.js
+++ src/html.js
@@ -3,16 +3,19 @@ import { typeOf, uuid } from "./utils.js";
 export function html(parts, ...args) {
   const template = parts.reduce(
     (acc, part, i) => {
-      if (part === null) {
-        return { events: acc.events, string: "" };
-      }
-
       // if no string put in first part
       if (!acc.string) {
         return { events: acc.events, string: part };
       }
 
       const arg = args[i - 1];
+
+      if (arg === null) {
+        return {
+          events: acc.events,
+          string: acc.string
+        };
+      }
 
       if (typeOf(arg) === "function") {
         // hash the function string to make an id
